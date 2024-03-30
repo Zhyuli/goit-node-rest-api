2124c552c665ab5d43fd9f51f0be865c22faafe4
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-03-30 23:28:06 +0200

    contactsServices.js

---
+++ services/contactsServices.js
@@ -0,0 +1,40 @@
+import { promises as fs } from "fs";
+import { nanoid } from "nanoid";
+import path from "path";
+
+const contactsPath = path.join("db", "contacts.json");
+
+export async function listContacts() {
+  const readContacts = await fs.readFile(contactsPath);
+  return JSON.parse(readContacts);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.find((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
