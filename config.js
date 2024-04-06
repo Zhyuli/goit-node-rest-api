389776dc22a8e32f6611f551a28790e3d598c6b9
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-04-06 18:41:56 +0300

    config.js removed

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://YuliaZh:cjMpPRhYR7HUVbsR@cluster0.mcbdydp.mongodb.net/bd-contacts?retryWrites=true&w=majority&appName=Cluster0";
-
-module.exports = {
-  DB_HOST,
-};
