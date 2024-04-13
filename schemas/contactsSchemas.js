29bfd677eb3991a4e758d65ff6650fd2f69fc562
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-04-13 16:57:30 +0300

    models +user.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,4 +1,5 @@
 import Joi from "joi";
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export const createContactSchema = Joi.object({
   name: Joi.string().required(),
@@ -15,4 +16,15 @@ export const updateContactSchema = Joi.object({
 
 export const updateFavouriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
+});
+
+export const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
 });
