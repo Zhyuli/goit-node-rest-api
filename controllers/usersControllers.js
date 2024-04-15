73e0f348193373a89c8e7ff3aea067385b303220
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-04-15 14:18:55 +0300

    409 Email in use

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,14 @@
 import { User } from "../models/user.js";
 
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
 const registerUser = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
   const newUser = await User.create(req.body);
 
   res.status(201).json({
