3b6ede1967f920763cd9598405364b85e64bf3f4
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-04-16 18:31:52 +0300

    auth findById_corrected

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -14,7 +14,7 @@ const authenticate = async (req, res, next) => {
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    const user = await User.findbyId(id);
+    const user = await User.findById(id);
     console.log("user", user);
 
     if (!user || !user.token || user.token !== token) {
